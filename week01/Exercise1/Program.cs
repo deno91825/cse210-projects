277d46e8334333a3bf5c5eb1ea6ba3d10f9805c8
deno91825 <denisogola123@gmail.com>
2026-03-08 17:47:46 +0300

    made corrections to assignment 1 and 4

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,13 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name ");
+        Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+        Console.Write("");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
     
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -9,7 +9,7 @@ class Program
         int total = 0;
         int items = 0;
         int largest = 0;
-        int average;
+        float average;
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         while (newNumber != 0)
         {
@@ -29,7 +29,7 @@ class Program
 
             }
         }
-        average = total / items;
+        average = (float)total / items;
         Console.WriteLine($"The sum is: {total}");
         Console.WriteLine($"The average is: {average}");
         Console.WriteLine($"The largest number is:{largest}");
