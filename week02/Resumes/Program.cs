db7ebdbed88ee372a235f7a1eba4b864ae6dbc95
deno91825 <denisogola123@gmail.com>
2026-03-09 12:31:26 +0300

    resumes done

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._endYear = 2022;
+        job1._startYear = 2018;
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Computer technician";
+        job2._endYear = 2025;
+        job2._startYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Oweki Denis";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
     }
 }
